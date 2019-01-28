a910cb4cc4b154d970e40b5e3c754746dd4dd425
Aman <rumman.djuice1@gmail.com>
2019-01-28 22:54:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent='app-quote'">Quote</button>
+                <button @click="selectedComponent='app-author'">Author</button>
+                <button @click="selectedComponent='app-new'">New</button>
+
+                <component></component>
+
+              <!-- <app-quote>
+
+                   <h1 slot="title">{{parentQuote}}</h1>
+                   <p slot="content">A wonderful Quote</p>
+                   <p>Second wonderful quote</p>
+                   <p slot="extra">Extra wonderful quote</p>
+
+               </app-quote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
+
+    export default {
+        components:{
+            'app-quote':Quote,
+            'app-author'Author,
+            'app-new':New,
+        },
+        data(){
+            return{
+                parentQuote:'The Quote',
+                selectedComponent:'app-quote',
+            };
+        }
+    }
+</script>
+
+<style>
+    h1{
+        color: green;
+    }
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="title">
+            <slot name="title"></slot>
+        </div>
+        <hr>
+        <div>
+            <slot name="content"></slot>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+        <div style="color: red;">
+            <slot name="extra">this is default text</slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            //quote:String,
+        }
+    }
+</script>
+
+<style scoped>
+    div{
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 5px black;
+        padding: 30px;
+        margin:30px auto;
+        text-align: center;
+    }
+    h1{
+        color: red;
+    }
+    .title{
+        font-style: italic;
+    }
+</style>
